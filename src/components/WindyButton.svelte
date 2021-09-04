e66b1881dcfeea7f09b07d577c5d7b0ea15337e8
CuriousCorrelation <CuriousCorrelation@protonmail.com>
2021-09-05 02:16:14 +0530

    Add `WindyButton` component

---
+++ src/components/WindyButton.svelte
@@ -0,0 +1,19 @@
+<script>
+	export let buttonText = 'Click me!';
+	// Passing in props
+</script>
+
+<!-- Svelte event forwarding -->
+<button on:click>
+	{buttonText}
+</button>
+
+<!-- Notice how you don't need to wrap the button in a div! -->
+<style>
+	button {
+		@apply bg-black text-white;
+		@apply p-2 m-2;
+		@apply font-bold;
+		@apply border-2 rounded-md border-gray-200;
+	}
+</style>
